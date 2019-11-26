84d46f03c1ac59fa914cf203c8ce9ede6adbedf1
Andreas Wass <wass.andreas@gmail.com>
2019-11-26 09:10:17 +0100

    Add some sane constructors
    A normal use-case is to create a stack context from an array
    containing some type.

--- include/uboost/context/stack_context.hpp
+++ include/uboost/context/stack_context.hpp
@@ -5,6 +5,7 @@
 #endif
 
 #include <cstdint>
+#include <array>
 
 namespace uboost
 {
@@ -14,6 +15,13 @@ struct stack_context
 {
     std::size_t size = 0;
     void *sp = nullptr;
+
+    stack_context() noexcept = default;
+    template<class T, std::size_t N>
+    stack_context(T (&sp)[N]) noexcept: size(N*sizeof(T)), sp(sp) {}
+    template<class T, std::size_t N>
+    stack_context(std::array<T, N> &arr): size(N*sizeof(T)), sp(arr.data()) {}
+    stack_context(std::size_t size, void *sp) noexcept : size(size), sp(sp) {}
 
 #if defined(UBOOST_USE_BOOST)
     operator boost::context::stack_context() noexcept
